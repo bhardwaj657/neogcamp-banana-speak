83f1e662b2e6c7ffac707a206f6133c150bbeecf
Shubham <shubham2002bharat@gmail.com>
2020-12-06 03:41:38 +0530

    conversion

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-var username = prompt("give me your name??")
-var welcomeMessage = "this scripts work!!" + username
-alert(welcomeMessage);
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+// outputDiv.innerText= "hi i am shubham"
+
+function clickEventHandler(){
+  outputDiv.innerText = "jeijeijijfij  " + txtInput.value;  
+};
+btnTranslate.addEventListener("click", clickEventHandler)
